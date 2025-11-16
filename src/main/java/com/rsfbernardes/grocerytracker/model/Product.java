22ec91d468fde3d93f61f6b3ddafd8627bce7c0c
¨Rodrigo <rsfbernardes@hotmail.com>
2025-11-15 22:15:36 -0300

    Product and ProductRepository

---
+++ src/main/java/com/rsfbernardes/grocerytracker/model/Product.java
@@ -0,0 +1,26 @@
+package com.rsfbernardes.grocerytracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String category;
+
+    private Boolean active = true;
+
+}
---
+++ src/main/java/com/rsfbernardes/grocerytracker/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.rsfbernardes.grocerytracker.repository;
+
+import com.rsfbernardes.grocerytracker.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByNameIgnoreCase(String name);
+
+}
