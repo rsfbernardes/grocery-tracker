49cb6eb8ff5156bddcd5fa12543edf0a15105cc3
¨Rodrigo <rsfbernardes@hotmail.com>
2025-11-15 21:48:46 -0300

    ElasticsearchConfig

---
+++ src/main/java/com/rsfbernardes/grocerytracker/elastic/ElasticsearchConfig.java
@@ -0,0 +1,19 @@
+package com.rsfbernardes.grocerytracker.elastic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+
+    @Override
+    @NonNull
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+}
