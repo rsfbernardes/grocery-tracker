c888d5256f1e697c536842b06fe1b49344a1dd43
¨Rodrigo <rsfbernardes@hotmail.com>
2025-11-17 11:57:43 -0300

    New configurations to run the app

--- src/main/java/com/rsfbernardes/grocerytracker/GroceryTrackerApplication.java
+++ src/main/java/com/rsfbernardes/grocerytracker/GroceryTrackerApplication.java
@@ -2,8 +2,10 @@ package com.rsfbernardes.grocerytracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "com.rsfbernardes.grocerytracker.elastic")
 public class GroceryTrackerApplication {
 
     public static void main(String[] args) {
